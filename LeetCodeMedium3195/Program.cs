9a5a3bb5fa64a3f47592d0ff1d81badc15be2147
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-08-01 20:41:43 +0100

    Add project files.

---
+++ LeetCodeMedium3195/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeMedium3195
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1 = Solution.MinimumArea([[0, 1, 0], [1, 0, 1]]);
+            if (result1 != 6)
+            {
+                Console.WriteLine($"result1 is: {result1} instead of 6");
+                return;
+            }
+            int result2 = Solution.MinimumArea([[1, 0], [0, 0]]);
+            if (result2 != 1)
+            {
+                Console.WriteLine($"result2 is: {result2} instead of 1");
+                return;
+            }
+
+            Console.WriteLine($"it works perfectly!");
+        }
+    }
+}
---
+++ LeetCodeMedium3195/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMedium3195
+{
+    public static class Solution
+    {
+        public static int MinimumArea(int[][] grid)
+        {
+            int minR = int.MaxValue;
+            int maxR = int.MinValue;
+            int minC = int.MaxValue;
+            int maxC = int.MinValue;
+
+            for (int i = 0; i < grid.Length; i++)
+                for (int j = 0; j < grid[0].Length; j++)
+                    if (grid[i][j] == 1)
+                    {
+                        minR = Math.Min(minR, i);
+                        maxR = Math.Max(maxR, i);
+                        minC = Math.Min(minC, j);
+                        maxC = Math.Max(maxC, j);
+                    }
+
+            return (maxR - minR + 1) * (maxC - minC + 1);
+        }
+    }
+}
